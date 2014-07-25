d8a0ad3fd8c32318792d8eed970143425ddc4e60
DannyArends <danny.arends@gmail.com>
2014-07-25 16:06:17 +0200

    Updates to the RNA-seq analysis

--- Mouse/RNASequencing/annotations.R
+++ Mouse/RNASequencing/annotations.R
@@ -17,9 +17,8 @@ colnames(RPKM) <- gsub(".", "-", gsub("RPKM_", "", colnames(RPKM)), fixed=TRUE) 
 
 annotation <- read.csv("SampleAnnotation.txt", sep="\t", header=TRUE)                                             # Sample annotation
 
-bio.mart <- useMart("ensembl", dataset="mmusculus_gene_ensembl")                                                  # Biomart for mouse genes
-
 if(!file.exists("BiomartAnnotation.txt")){
+  bio.mart <- useMart("ensembl", dataset="mmusculus_gene_ensembl")                                                  # Biomart for mouse genes
   ENSMUSG <- as.character(probeannotation[,1])                                                                    # Ensemble genes we want to retrieve
   biomartResults <- NULL
   for(x in seq(0, length(ENSMUSG), 1000)){                                                                        # Do 1000 per time, just to please biomaRt
@@ -49,7 +48,7 @@ QD1 <- RPKM[,Qcross1]                                                           
 QD2 <- RPKM[,Qcross2]                                                                                                 # BFMI cross B6NxBFMI860-12 (D2)
 pval <- apply(cbind(QD1,QD2),1,function(x){ return(t.test(x[1:3], x[4:6])$p.value)})                                  # T-test for differences
 QF1 <- cbind(QD1, QD2, apply(QD1, 1, mean), apply(QD2, 1, mean), apply(QD1, 1, mean) / apply(QD2, 1, mean), pval)
-colnames(QF1)[7:10] <- c("Mean BFMI860-12xB6N", "Mean B6NxBFMI860-12", "Ratio", "tTest")
+colnames(QF1)[7:10] <- c("Mean BFMI860-12xB6N Q", "Mean B6NxBFMI860-12 Q", "Ratio", "tTest")
 
 # Liver
 Lcross1 <- c("F1-V-1004_L", "F1-V-1016_L", "F1-V-1020_L")
@@ -58,7 +57,7 @@ LD1 <- RPKM[,Lcross1]                                                           
 LD2 <- RPKM[,Lcross2]                                                                                                 # BFMI cross B6NxBFMI860-12 (D2)
 pval <- apply(cbind(LD1,LD2),1,function(x){ return(t.test(x[1:3], x[4:6])$p.value)})                                  # T-test for differences
 LF1 <- cbind(LD1, LD2, apply(LD1, 1, mean), apply(LD2, 1, mean), apply(LD1, 1, mean) / apply(LD2, 1, mean), pval)
-colnames(LF1)[7:10] <- c("Mean BFMI860-12xB6N", "Mean B6NxBFMI860-12", "Ratio", "tTest")
+colnames(LF1)[7:10] <- c("Mean BFMI860-12xB6N L", "Mean B6NxBFMI860-12 L", "Ratio", "tTest")
 
 LBFMIA  <- c("86026522_L", "86026502_L")
 LB6NA   <- c("1006954_L", "1006956_L")
@@ -79,18 +78,17 @@ write.table(updatedData, file="BFMI_RPKM_ANN.txt", sep="\t", row.names=FALSE)
 
 # Quality control of the data
 
-RPKM_MEAN <- apply(RPKM, 1, mean)
-
-notExpressed <- which(RPKM_MEAN <= 1)
-RPKM <- RPKM[-notExpressed, ]
-cat("Filtered out:", length(notExpressed), "genes (not expressed)\n")
+#RPKM_MEAN <- apply(RPKM, 1, mean)
+#notExpressed <- which(RPKM_MEAN <= 1)
+#RPKM <- RPKM[-notExpressed, ]
+#cat("Filtered out:", length(notExpressed), "genes (not expressed)\n")
 
 QD1   <- RPKM[,Qcross1]  ; QD2   <- RPKM[,Qcross2]     # Create new subsets, because we reduced the amount of data
 LD1   <- RPKM[,Lcross1]  ; LD2   <- RPKM[,Lcross2]
 LBFMI <- RPKM[,LBFMIA]   ; LB6N  <- RPKM[,LB6NA]
 
 # Subset the data
-findGroups <- function(BFMI, B6N, F1, pExp=0.1, p = 0.05){
+findGroups <- function(BFMI, B6N, F1, pExp=0.1, p = 0.1){
   # Find differentially expressed genes between parents
   pval_B6N_E_BFMI <- apply(cbind(BFMI, B6N), 1, function(x){ return(t.test(x[1:2], x[3:4])$p.value)})
   B6N_D_BFMI      <- which(pval_B6N_E_BFMI < pExp)
@@ -119,38 +117,61 @@ findGroups <- function(BFMI, B6N, F1, pExp=0.1, p = 0.05){
   GU1 <-  pval_B6N_S_BFMI < 0.1 & pval_B6N_S >= p & pval_BFMI_L >= p; wGU1 <- which(GU1); cat("Group U1: B6N == D1/D2 == BFMI:", length(which(GU1)), "\n")
 
   G4  <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L <  p & pval_BFMI_S <  p; wG4 <- which(G4);   cat("Group  4: BFMI  < D1/D2  < B6N:", length(wG4), "\n")
-  G5  <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L >= p & pval_BFMI_S <  p; wG5 <- which(G5);   cat("Group  5: BFMI == D1/D2  < B6N:", length(wG5), "\n")
-  G6  <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L <  p & pval_BFMI_S >= p; wG6 <- which(G6);   cat("Group  6: BFMI  < D1/D2 == B6N:", length(wG6), "\n")
+  G5  <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L <  p & pval_BFMI_S >= p; wG5 <- which(G5);   cat("Group  5: BFMI == D1/D2  < B6N:", length(wG5), "\n")
+  G6  <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L >= p & pval_BFMI_S <  p; wG6 <- which(G6);   cat("Group  6: BFMI  < D1/D2 == B6N:", length(wG6), "\n")
   GU2 <-  pval_B6N_L_BFMI < 0.1 & pval_B6N_L >= p & pval_BFMI_S >= p; wGU2 <- which(GU2); cat("Group U2: BFMI == D1/D2 == B6N:", length(which(GU2)), "\n")
-  return(list(B6N_F1_BFMI[G1,], B6N_F1_BFMI[G2,], B6N_F1_BFMI[G3,], B6N_F1_BFMI[GU1,], B6N_F1_BFMI[G4,], B6N_F1_BFMI[G5,], B6N_F1_BFMI[G6,], B6N_F1_BFMI[GU2,],
+  return(list(B6N_F1_BFMI[wG1,], B6N_F1_BFMI[wG2,], B6N_F1_BFMI[wG3,], B6N_F1_BFMI[wGU1,], B6N_F1_BFMI[wG4,], B6N_F1_BFMI[wG5,], B6N_F1_BFMI[wG6,], B6N_F1_BFMI[wGU2,],
               list(pval_B6N_S_BFMI, pval_B6N_L_BFMI, pval_B6N_S, pval_B6N_L, pval_BFMI_S, pval_BFMI_L)))
 }
 
 updatedData <- cbind(updatedData, "A/D_BFMI860-12xB6N" = rep(NA, nrow(updatedData)))
+updatedData <- cbind(updatedData, "A/D_B6NxBFMI860-12" = rep(NA, nrow(updatedData)))
+updatedData <- cbind(updatedData, "E_A_BFMI860-12xB6N_B6N" = rep(NA, nrow(updatedData)))
+updatedData <- cbind(updatedData, "E_D_BFMI860-12xB6N_B6N" = rep(NA, nrow(updatedData)))
+updatedData <- cbind(updatedData, "E_A_B6NxBFMI860-12_B6N" = rep(NA, nrow(updatedData)))
+updatedData <- cbind(updatedData, "E_D_B6NxBFMI860-12_B6N" = rep(NA, nrow(updatedData)))
 
-dim(updatedData)
 analysis_LD1 <- findGroups(LBFMI, LB6N, LD1)
 updatedData[as.numeric(rownames(LD1)), "A/D_BFMI860-12xB6N"] <- "-"
-dim(updatedData)
+updatedData[as.numeric(rownames(LD2)), "E_A_BFMI860-12xB6N_B6N"] <- "-"
+updatedData[as.numeric(rownames(LD2)), "E_D_BFMI860-12xB6N_B6N"] <- "-"
+alleffectsD1 <- updatedData[,"Mean BFMI860-12xB6N L"] - updatedData[,"Mean B6N"]
+
 updatedData[as.numeric(rownames(analysis_LD1[[1]])), "A/D_BFMI860-12xB6N"] <- "ADDITIVE"
+updatedData[as.numeric(rownames(analysis_LD1[[1]])), "E_A_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[1]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[2]])), "A/D_BFMI860-12xB6N"] <- "B6N"
+updatedData[as.numeric(rownames(analysis_LD1[[2]])), "E_D_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[2]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[3]])), "A/D_BFMI860-12xB6N"] <- "BFMI"
+updatedData[as.numeric(rownames(analysis_LD1[[3]])), "E_D_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[3]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[4]])), "A/D_BFMI860-12xB6N"] <- "UNKNOWN"
 updatedData[as.numeric(rownames(analysis_LD1[[5]])), "A/D_BFMI860-12xB6N"] <- "ADDITIVE"
+updatedData[as.numeric(rownames(analysis_LD1[[5]])), "E_A_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[5]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[6]])), "A/D_BFMI860-12xB6N"] <- "BFMI"
+updatedData[as.numeric(rownames(analysis_LD1[[6]])), "E_D_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[6]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[7]])), "A/D_BFMI860-12xB6N"] <- "B6N"
+updatedData[as.numeric(rownames(analysis_LD1[[7]])), "E_D_BFMI860-12xB6N_B6N"] <- alleffectsD1[as.numeric(rownames(analysis_LD1[[7]]))]
 updatedData[as.numeric(rownames(analysis_LD1[[8]])), "A/D_BFMI860-12xB6N"] <- "UNKNOWN"
 
-updatedData <- cbind(updatedData, "A/D_B6NxBFMI860-12" = rep(NA, nrow(updatedData)))
+
 analysis_LD2 <- findGroups(LBFMI, LB6N, LD2)
 updatedData[as.numeric(rownames(LD2)), "A/D_B6NxBFMI860-12"] <- "-"
+updatedData[as.numeric(rownames(LD2)), "E_A_B6NxBFMI860-12_B6N"] <- "-"
+updatedData[as.numeric(rownames(LD2)), "E_D_B6NxBFMI860-12_B6N"] <- "-"
+alleffectsD2 <- updatedData[,"Mean B6NxBFMI860-12 L"] - updatedData[,"Mean B6N"]
+
 updatedData[as.numeric(rownames(analysis_LD2[[1]])), "A/D_B6NxBFMI860-12"] <- "ADDITIVE"
+updatedData[as.numeric(rownames(analysis_LD2[[1]])), "E_A_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[1]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[2]])), "A/D_B6NxBFMI860-12"] <- "B6N"
+updatedData[as.numeric(rownames(analysis_LD2[[2]])), "E_D_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[2]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[3]])), "A/D_B6NxBFMI860-12"] <- "BFMI"
+updatedData[as.numeric(rownames(analysis_LD2[[3]])), "E_D_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[3]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[4]])), "A/D_B6NxBFMI860-12"] <- "UNKNOWN"
 updatedData[as.numeric(rownames(analysis_LD2[[5]])), "A/D_B6NxBFMI860-12"] <- "ADDITIVE"
+updatedData[as.numeric(rownames(analysis_LD2[[5]])), "E_A_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[5]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[6]])), "A/D_B6NxBFMI860-12"] <- "BFMI"
+updatedData[as.numeric(rownames(analysis_LD2[[6]])), "E_D_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[6]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[7]])), "A/D_B6NxBFMI860-12"] <- "B6N"
+updatedData[as.numeric(rownames(analysis_LD2[[7]])), "E_D_B6NxBFMI860-12_B6N"] <- alleffectsD2[as.numeric(rownames(analysis_LD2[[7]]))]
 updatedData[as.numeric(rownames(analysis_LD2[[8]])), "A/D_B6NxBFMI860-12"] <- "UNKNOWN"
 
 write.table(updatedData, file="BFMI_RPKM_ANN_AddDom.txt", sep="\t", row.names=FALSE)

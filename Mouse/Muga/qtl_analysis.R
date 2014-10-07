7707d5b2f55735e60c613ea73141d9f77bd13ca8
DannyArends <danny.arends@gmail.com>
2014-10-07 14:20:14 +0200

    Working on the QTL stuff, added correction for chromosome 3, doesn't change much

--- Mouse/Muga/qtl_analysis.R
+++ Mouse/Muga/qtl_analysis.R
@@ -30,7 +30,7 @@ mriGWAS <- function(genotypes, phenotypes, pheno.col = "42d", to = nrow(genotype
     genotype      <- as.factor(t(genotypes[x,!is.na(genotypes[x,])]))
     littersize    <- as.numeric(phenotypes[ind, "WG2"])
     phenotype     <- phenotypes[ind, paste0("mri",pheno.col,"_fat")] / phenotypes[ind, paste0("mri",pheno.col,"_lean")]
-    tryCatch(res  <- anova(lm(phenotype ~  littersize + genotype))[[5]][2], error = function(e){ res <<- NA })
+    tryCatch(res  <- anova(lm(phenotype ~ littersize + genotype))[[5]][2], error = function(e){ res <<- NA })
     pvalues <- c(pvalues, res)
   }
   plot(-log10(pvalues),t='l')
@@ -48,8 +48,49 @@ write.table(cbind(qtl42,   qtl56,   qtl70), "Analysis/qtls_fatDlean_gwas.txt", s
 write.table(cbind(qtlPH42, qtlPH56, qtlPH70), "Analysis/qtls_fatDlean_gwasPH.txt", sep="\t")
 write.table(cbind(qtlGP42, qtlGP56, qtlGP70), "Analysis/qtls_fatDlean_gwasGP.txt", sep="\t")
 
-qtls <- read.table("Analysis/qtls_fatDlean_gwas.txt", sep="\t", colClasses=c("character",rep("numeric",3)), header=TRUE,row.names=FALSE)
+qtls <- read.table("Analysis/qtls_fatDlean_gwas.txt", sep="\t", colClasses=c("character",rep("numeric",3)), header=TRUE)
 
+image(x=1:nrow(qtls), y=(1:3)-0.5, as.matrix(qtls),oldstyle=TRUE, breaks=c(0,3,6,12,30,100), col=c("white",gray.colors(4)[4:1]))
+grid(3)
+box()
+
+setwd("E:/Mouse/ClassicalPhenotypes/AIL")
+chrcolors <- rep(c("black","orange"),length(unique(map[,"Chr"])))
+names(chrcolors) <- unique(map[,"Chr"])
+
+png("Plots/QTL_FatdLean_Day42.png", width=1024, height=768)
+  plot(qtls[,"qtl42"], t='h', col=chrcolors[map[,"Chr"]],main="Fat/Lean QTL profile Day 42", xlab="Marker", ylab="-log10(p-value)")
+  abline(h = -log10(0.1/nrow(qtls)), col="orange", lty=2); abline(h = -log10(0.05/nrow(qtls)), col="gold", lty=2); abline(h = -log10(0.01/nrow(qtls)), col="green", lty=2)
+dev.off()
+
+png("Plots/QTL_FatdLean_Day56.png", width=1024, height=768)
+  plot(qtls[,"qtl56"], t='h', col=chrcolors[map[,"Chr"]],main="Fat/Lean QTL profile Day 56", xlab="Marker", ylab="-log10(p-value)")
+  abline(h = -log10(0.1/nrow(qtls)), col="orange", lty=2); abline(h = -log10(0.05/nrow(qtls)), col="gold", lty=2); abline(h = -log10(0.01/nrow(qtls)), col="green", lty=2)
+dev.off()
+
+png("Plots/QTL_FatdLean_Day70.png", width=1024, height=768)
+  plot(qtls[,"qtl70"], t='h', col=chrcolors[map[,"Chr"]],main="Fat/Lean QTL profile Day 70", xlab="Marker", ylab="-log10(p-value)")
+  abline(h = -log10(0.1/nrow(qtls)), col="orange", lty=2); abline(h = -log10(0.05/nrow(qtls)), col="gold", lty=2); abline(h = -log10(0.01/nrow(qtls)), col="green", lty=2)
+dev.off()
+
+mriGWAS_Cof <- function(genotypes, phenotypes, pheno.col = "42d", to = nrow(genotypes)){                                            # Use the chromosome 3 locus as cofactor
+  pvalues <- NULL
+  for(x in 1:to){
+    ind           <- colnames(genotypes[x,!is.na(genotypes[x,])])
+    genotype      <- as.factor(t(genotypes[x,!is.na(genotypes[x,])]))
+    cof           <- as.factor(t(genotypes["UNC5048297", ind]))
+    littersize    <- as.numeric(phenotypes[ind, "WG2"])
+    phenotype     <- phenotypes[ind, paste0("mri",pheno.col,"_fat")] / phenotypes[ind, paste0("mri",pheno.col,"_lean")]
+    tryCatch(res  <- anova(lm(phenotype ~ littersize + cof + genotype))[[5]][3], error = function(e){ res <<- NA })
+    pvalues <- c(pvalues, res)
+  }
+  plot(-log10(pvalues),t='l')
+  names(pvalues) <- rownames(genotypes)[1:to]
+  return(-log10(pvalues))
+}
+qtl42cof   <- mriGWAS_Cof(genotypes, phenotypes, "42d")
+plot(qtl42cof, t='h', col=chrcolors[map[,"Chr"]],main="Fat/Lean QTL profile Day 70", xlab="Marker", ylab="-log10(p-value)")
+  
 chromosomes  <- as.character(c(1:19, "X", "Y", "M"))
 setwd("E:/Mouse/DNA/DiversityArray/")
 chrInfo      <- read.table("Annotation/mouseChrInfo.txt", header=TRUE)
@@ -66,6 +107,13 @@ aa <- lapply(rownames(qtls), function(x){
   points(x=xloc + (LOD/100), y=yloc , pch=15,cex=0.5)
 })
 
+#  Regions found in QTL mapping:
+# Chr   Start     Stop   42 56 70
+#  3  32392808  54702204  V  V  V
+#  4 143353391 143419321  V
+#  9  70272613 103351100     V  V
+# 16  69339204  71889002  V  V  V
+#  X  74247872  74247872  V
 
 cnt <- 1
 aa <- apply(chrInfo,1,function(x){

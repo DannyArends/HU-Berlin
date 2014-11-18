d62154789d0d2c11e0ead3fcf15986ae28e37d30
DannyArends <danny.arends@gmail.com>
2014-11-18 16:28:42 +0100

    Comparing the normalized values back to MPI

--- Mouse/RNASequencing/MPIcomparison.R
+++ Mouse/RNASequencing/MPIcomparison.R
@@ -9,14 +9,22 @@
 setwd("E:/Mouse/RNA/Sequencing/Reciprocal Cross B6 BFMI by MPI")
 
 sampleIDs <- read.table("FASTQ/sampledescription.txt",sep="\t", header=TRUE)
+RPKM      <- read.csv("Analysis/RPKMnorm.txt", sep="\t", check.names=FALSE, header=TRUE, row.names=1)                             # RNA-Seq summary data
+cat("Called expressions for", nrow(RPKM), "genes\n")                                                                              # Called expressions for 41388 genes
+RPKM_MPI  <- read.csv("MPI_RPKM_ANALYSIS/2014-07-04_BFMI_RPKM.txt", check.names=FALSE, sep="\t", header=TRUE, row.names=1)        # RNA-Seq summary data from MDC
+cat("MPI called expressions for", nrow(RPKM_MPI), "genes\n")                                                                      # MPI called expressions for 23330 genes
 
-RPKM_MPI <- read.csv("MPI_RPKM_ANALYSIS/2014-07-04_BFMI_RPKM.txt", sep="\t", header=TRUE, row.names=1)       # RNA-Seq summary data from MDC
-cat("MPI called expressions for", nrow(RPKM_MPI), "genes\n")
+libraryIDs  <- as.numeric(unlist(lapply(strsplit(colnames(RPKM),"_"),"[",1)))
+RPKM        <- RPKM[,which(libraryIDs %in% sampleIDs$Lib_id)]
+libraryIDs  <- libraryIDs[which(libraryIDs %in% sampleIDs$Lib_id)]
 
-corenames <- as.character(sampleIDs[match(as.numeric(unlist(lapply(strsplit(colnames(RPKM),"_"),"[",1))), sampleIDs$Lib_id), "core_name"])
-corenames <- unlist(lapply(strsplit(corenames,"_"),"[",1))
+orderInSampleID <- match(libraryIDs, sampleIDs$Lib_id)
+corenames       <- as.character(sampleIDs[orderInSampleID, "core_name"])
+RPKM            <- RPKM[,orderInSampleID]
+corenames       <- unlist(lapply(lapply(strsplit(corenames,"_"),"[",c(1:2)), function(x){ paste0(x[1],"_",x[2]) }))
 
-shortnames <- unlist(lapply(strsplit(colnames(RPKM),"_"),"[",1))
+colnames(RPKM)      <- corenames
+colnames(RPKM_MPI)  <- gsub("RPKM_","",colnames(RPKM_MPI))
 
 RPKM_MPI <- RPKM_MPI[, unlist(lapply(corenames, grep, colnames(RPKM_MPI)))]
 

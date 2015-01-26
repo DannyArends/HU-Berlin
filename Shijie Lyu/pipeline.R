e8f83da1f3e3b2d055523c61298f926a34093143
DannyArends <danny.arends@gmail.com>
2015-01-26 16:12:03 +0100

    Pipeline for DNA Sequencing data n chicken, updates

--- Shijie Lyu/pipeline.R
+++ Shijie Lyu/pipeline.R
@@ -11,6 +11,7 @@ execute <- function(x, intern = FALSE){
   cat("----", x, "\n")
   res <- system(x, intern = intern)
   cat(">>>>", res[1], "\n")
+  if(res[1] >= 1) q("no")
 }
 
 referenceDir  <- "genomes"
@@ -38,7 +39,7 @@ cmdBase <- paste0("java -jar ", trimmomatic, "trimmomatic-0.32.jar PE")
 params  <- paste0("ILLUMINACLIP:", trimmomatic, "adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36")
 
 command <- paste(cmdBase, inputfiles[1], inputfiles[2], outputfiles[1], outputfiles[2], outputfiles[3], outputfiles[4], params)
-#execute(command)
+execute(command)
 
 ### BWA: Alignment against genome (2 to 8 hours), we pipe the output to the next step ###
 # -v 2  : Verbose level 2 only errors and warnings
@@ -54,48 +55,53 @@ command     <- paste0(command, "samtools view -Sb - | ")
 # -m : Memory per CPU core
 outputSBAM   <- paste0(fileBase, "P_trimmed.aligned.sorted.bam")
 command      <- paste0(command, "samtools sort -@ 4 -m 2G -o - ", fileBase, " > ", outputSBAM)
-#execute(command)
+execute(command)
 
 ### Add a read group ###
 outputSRGBAM <- paste0(fileBase, "P_trimmed.aligned.sorted.rg.bam")
-IDcode       <- substr(fileBase, 1, 4)
-command      <- paste0("java -Xmx4g -jar ", picard, "AddOrReplaceReadGroups.jar INPUT=", outputSBAM, " OUTPUT=", outputSRGBAM," CREATE_INDEX=false RGID=",IDcode," RGLB=LIB",IDcode," RGPL=Illumina RGPU=X RGSM=", IDcode)
-#execute(command)
+IDcode       <- substr(fileBase, 8, 12)
+command      <- paste0("java -Xmx4g -jar ", picard, "AddOrReplaceReadGroups.jar INPUT=", outputSBAM, " OUTPUT=", outputSRGBAM, " CREATE_INDEX=false RGID=", IDcode, " RGLB=LIB", IDcode, " RGPL=Illumina RGPU=X RGSM=", IDcode)
+execute(command)
 
 ### Move the file with the read group over the previous file ###
 command <- paste0("mv ", outputSRGBAM, " ", outputSBAM)
-#execute(command)
+execute(command)
 
 ### Index the BAM file (10 minutes) ###
 outputSBAI   <- paste0(fileBase, "P_trimmed.aligned.sorted.bai")
 command      <- paste0("samtools index ", outputSBAM, " ", outputSBAI)
-#execute(command)
+execute(command)
 
 ### Mark duplicates, using the Picard tools (~ 30 minutes) ###
 outputSBAID    <- paste0(fileBase, "P_trimmed.aligned.sorted.dedup.bam")
-outputMetrics  <- paste0(fileBase, "matrics.txt")
-command        <- paste0("java -jar ", picard, "MarkDuplicates.jar INPUT=", outputSBAM, " OUTPUT=", outputSBAID," METRICS_FILE=", outputMetrics)
-#execute(command)
+outputMetrics  <- paste0(fileBase, ".metrics.txt")
+command        <- paste0("java -jar ", picard, "MarkDuplicates.jar INPUT=", outputSBAM, " OUTPUT=", outputSBAID," METRICS_FILE=", outputMetrics," MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000")
+execute(command)
+
+### Index the BAM file (10 minutes) ###
+outputSBAIDI <- paste0(fileBase, "P_trimmed.aligned.sorted.dedup.bai")
+command      <- paste0("samtools index ", outputSBAID, " ", outputSBAIDI)
+execute(command)
 
 ### Get some basic statistics (5 to 10 minutes)
 command      <- paste0("samtools flagstat ", outputSBAID)
-#execute(command)
+execute(command)
 
 ### Indel Realign
 outputSNPS    <- "output.snp.intervals"
 outputSIBAM   <- paste0(fileBase, "P_trimmed.aligned.sorted.realigned.bam")
-knownindels   <- paste0(referenceDir, "/Gallus_gallus.vcf")                               # Reference, download from: ftp://ftp.ensembl.org/pub/release-78/variation/vcf/gallus_gallus/
+knownindels   <- paste0(referenceDir, "/Gallus_gallus.reordered.vcf")                               # Reference, download from: ftp://ftp.ensembl.org/pub/release-78/variation/vcf/gallus_gallus/
 if(!file.exists(outputSNPS)){
   command <- paste0("java -Xmx4g -jar ", gatk, " -nt 4 -T RealignerTargetCreator -R ", reference, " -known ", knownindels, " -o ", outputSNPS, " -U ALLOW_N_CIGAR_READS")
   execute(command)                                                           # Call the GATK RealignerTargetCreator, only need to be done because the knownSNPlist does not change
 }
-command <- paste0("java -Xmx4g -jar ", gatk, " -T IndelRealigner -R ", reference, " -targetIntervals ", outputSNPS, " -maxReads 150000 -I ", outputSBAID, " -o ",outputSIBAM," -known ",knownindels, " -U ALLOW_N_CIGAR_READS")
+command <- paste0("java -Xmx4g -jar ", gatk, " -T IndelRealigner -R ", reference, " -targetIntervals ", outputSNPS, " -I ", outputSBAID, " -o ",outputSIBAM," -known ",knownindels, " --consensusDeterminationModel KNOWNS_ONLY --fix_misencoded_quality_scores")
 execute(command)                                                             # Call the GATK IndelRealigner
 
 ### Base Recalibration
-knownsnps     <- paste0(referenceDir, "/Gallus_gallus.vcf")                               # Reference, download from: ftp://ftp.ensembl.org/pub/release-78/variation/vcf/gallus_gallus/
-covfile1      <- paste0(fileBase, "1.covariates")
-covfile2      <- paste0(fileBase, "2.covariates")
+knownsnps     <- paste0(referenceDir, "/Gallus_gallus.reordered.vcf")                               # Reference, download from: ftp://ftp.ensembl.org/pub/release-78/variation/vcf/gallus_gallus/
+covfile1      <- paste0(fileBase, ".1.covariates")
+covfile2      <- paste0(fileBase, ".2.covariates")
 plotfile      <- paste0(fileBase, "recalibration.pdf")
 outputSIRBAM  <- paste0(fileBase, "P_trimmed.aligned.sorted.realigned.recalibrated.bam")
 
@@ -107,3 +113,5 @@ command <- paste0("java -Xmx4g -jar ", gatk, " -nct 4 -T BaseRecalibrator -R ", 
 execute(command)                                                            # Call the GATK BaseRecalibrator
 command <- paste0("java -Xmx4g -jar ", gatk, " -T AnalyzeCovariates -R ", reference, " -before ", covfile1, " -after ", covfile2, " -U ALLOW_N_CIGAR_READS -plots ", plotfile)
 execute(command)                                                            # Call the GATK AnalyzeCovariates
+
+q("no")

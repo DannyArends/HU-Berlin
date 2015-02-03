be52a151d31bbd29c86807c4f45b6724df229461
DannyArends <danny.arends@gmail.com>
2015-02-03 14:55:52 +0100

    Adding the annotation to the array data, removing faulty arrays

--- Mouse/FV3/IlluminaBeadArray.R
+++ Mouse/FV3/IlluminaBeadArray.R
@@ -13,6 +13,8 @@ library(beadarray)
 setwd("E:/Mouse/RNA/FV3")
 arrays    <- read.table("Annotation/arrayannotation.txt", sep="\t", header=TRUE, row.names=1)                                  # Arrays annotation
 
+# Extract data from bead arrays
+
 for(beadarray in unique(arrays[,"array"])){
   metrics <- read.table(paste0("RawData/", beadarray, "/Metrics.txt"), sep = "\t", header = TRUE , as.is = TRUE)
   snr <- metrics$P95Grn / metrics$P05Grn
@@ -27,6 +29,8 @@ for(beadarray in unique(arrays[,"array"])){
   datasumm.c <- summarize(BLData = mydata, useSampleFac = TRUE, sampleFac=as.character(unlist(mydata@sectionData$SampleGroup)))
   write.table(exprs(datasumm.c), file = paste0("Intermediate/summary",beadarray,".txt"), sep="\t", row.names=TRUE)
 }
+
+# Reload the extracted files and combine them into a single expression matrix
 
 alldata <- vector("list", length(unique(arrays[,"array"])))
 cnt <- 1
@@ -46,6 +50,26 @@ for(beadarray in unique(arrays[,"array"])){
 }
 write.table(alldata, file="Intermediate/summaryArrays.txt", sep ="\t", row.names=TRUE)
 
+# After creating the files (also create the annotation matrix using createAnnotation.R)
+
+library(preprocessCore)                                                                                               # preprocessCore - For quantile normalisation
+
 setwd("E:/Mouse/RNA/FV3")
-rawdata <- read.table("Intermediate/summaryArrays.txt", sep = "\t", header=TRUE, check.names=FALSE)
-annotationmatrix <- read.table("Annotation/probeannotation.txt", sep="\t", header=TRUE)
+arrays <- read.table("Annotation/arrayannotation.txt", sep="\t", header=TRUE, row.names=1)                                  # Arrays annotation
+annotationmatrix <- read.table("Annotation/probeannotation.txt", sep="\t", header=TRUE)                                     # Probe annotation
+rawdata <- read.table("Intermediate/summaryArrays.txt", sep = "\t", header=TRUE, check.names=FALSE)                         # Raw data extracted from the arrays
+
+badarrays <- c("1562232038_E", "1562232037_A","1562232040_C", "1740174012_D", "1754402037_A")
+
+rawdata <- rawdata[, -which(colnames(rawdata) %in% badarrays)]                           # Remove the misbehaving arrays
+arrays <- arrays[-which(rownames(arrays) %in% badarrays),]                               # Remove the misbehaving arrays
+
+# Preprocessing
+normdata <- normalize.quantiles(as.matrix(rawdata))                                                                         # Quantile normalisation
+colnames(normdata) <- colnames(rawdata)
+rownames(normdata) <- rownames(rawdata)
+
+normdata <- normdata[which(rownames(normdata) %in% annotationmatrix[,"ProbeName"]),]
+
+alldata <- cbind(annotationmatrix[match(rownames(normdata), annotationmatrix[,"ProbeName"]),], normdata)[1:10,]
+write.table(alldata, file="Analysis/geneexpression.txt", sep="\t", row.names=FALSE)

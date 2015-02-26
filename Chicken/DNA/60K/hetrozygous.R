e5cc6c70307e3a6f6ce6cf5b5b7095abdfbcd67d
DannyArends <danny.arends@gmail.com>
2015-02-26 12:52:38 +0100

    Looking for hetrozygousity in chicken data

---
+++ Chicken/DNA/60K/hetrozygous.R
@@ -0,0 +1,5 @@
+# Analysis of missing heterozygous in the Mega Muga array
+#
+# copyright (c) 2014-2020 - Brockmann group - HU Berlin, Danny Arends
+# last modified Feb, 2015
+# first written Feb, 2015

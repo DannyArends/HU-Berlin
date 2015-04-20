4dce2e96cb71ceb35ee9b007730882e349f4ff2c
DannyArends <danny.arends@gmail.com>
2015-04-20 16:37:11 +0200

    Not working, still commiting

--- Mouse/DiversityArray/Wellcome-CTC.R
+++ Mouse/DiversityArray/Wellcome-CTC.R
@@ -23,16 +23,29 @@ if(!file.exists("CTC/Analysis/probes.fasta")){
   }
 }
 
-# blastn -task blastn -query CTC/Analysis/probes.fasta -db E:/Mouse/DNA/DiversityArray/Analysis/Mus_musculus.GRCm38.74.dna.db -perc_identity 100 -outfmt 6 -evalue=0.1 -out CTC/Analysis/probelocations.txt
+# blastn -task blastn -query CTC/Analysis/probes.fasta -db E:/Mouse/DNA/DiversityArray/Analysis/Mus_musculus.GRCm38.74.dna.db -perc_identity 95 -outfmt 6 -evalue=0.1 -out CTC/Analysis/probelocations.txt
 
 # Load the locations of the probes and filter them so that we only keep the unique matching probes
 locations <- read.csv("CTC/Analysis/probelocations.txt", sep = "\t", header=FALSE)
 colnames(locations) <- c("ProbeName", "Chr", "Ident", "Length", "U1", "U2", "U3", "Match", "Start", "Stop", "evalue", "Score")
 
-locations <- locations[-which(locations[,"Score"] < 80),]                                                                           # Match is not good enough to be considered as duplicate ( evalue < 80 )
+locations <- locations[-which(locations[,"Score"] < 60),]                                                                                       # Match is not good enough to be considered as duplicate ( evalue < 60 )
 
-dupprobes <- unique(locations[which(duplicated(locations[,"ProbeName"])),"ProbeName"])                                              # Probes which have multiple matches
-bestlocs <- locations[ which(!locations[,"ProbeName"] %in% dupprobes),]                                                             # No multi-mapping probes
+dupprobes   <- unique(locations[which(duplicated(locations[,"ProbeName"])),"ProbeName"])                                                        # Probes which have multiple matches
+goodprobes  <- locations[ which(!locations[,"ProbeName"] %in% dupprobes),]                                                                      # No multi-mapping probes
+goodprobes  <- goodprobes[, c("ProbeName", "Chr", "Start", "Stop")]
+rownames(goodprobes) <- goodprobes[,1]
+goodprobes <- goodprobes[,-1]
+goodprobes <- cbind(goodprobes, Location = (goodprobes[,"Start"] + goodprobes[,"Stop"])/2)
+
+refalt <- matrix("", nrow(goodprobes), 2, dimnames=list(rownames(goodprobes), c("Ref","Alt")))
+for(x in rownames(goodprobes)){
+  probesequence <- unlist(strsplit(wellcomeannot[gsub("OLD","",x), "Sequence"],""))
+  refalt[x, "Alt"] <- probesequence[(which(probesequence == "[")+1)] ; refalt[x, "Ref"] <- probesequence[(which(probesequence == "]")-1)]
+}
+
+goodprobes <- cbind(goodprobes, refalt)
+
 
 if(!file.exists("CTC/Analysis/alldata.txt")){
   snpdata1 <- read.table("CTC/RAW/Strains-0.09052005.txt", sep=" ", header=TRUE, colClasses="character", row.names=1, check.names=FALSE)
@@ -43,7 +56,7 @@ if(!file.exists("CTC/Analysis/alldata.txt")){
 }else{
   wellcomedata <- read.table(file="CTC/Analysis/alldata.txt", sep="\t", header = TRUE, row.names=1, colClasses="character", check.names=FALSE)
 }
-cat("Wellcome data loaded:", ncol(wellcomedata), "strains,", nrow(wellcomedata), "SNPs\n")                                          # Wellcome data loaded: 481 strains, 13368 SNPs
+cat("Wellcome data loaded:", ncol(wellcomedata), "strains,", nrow(wellcomedata), "SNPs\n")                                                      # Wellcome data loaded: 481 strains, 13368 SNPs
 
 nwellcome <- matrix(NA, nrow(wellcomedata), ncol(wellcomedata) + 2, 
                     dimnames = list(rownames(wellcomedata), c("Chr", "Location", "CTC_A", "CTC_B", colnames(wellcomedata)[-c(1,2)])))
@@ -52,27 +65,44 @@ for(x in 1:nrow(wellcomedata)){
   nwellcome[x,"Chr"] <- as.character(wellcomedata[x,"CHR"])
   nwellcome[x,"Location"] <- as.character(wellcomedata[x,"POSITION"])
   gtype <- unique(unlist(wellcomedata[x, 3:ncol(wellcomedata)]))
+  gtypes <- as.character(wellcomedata[x, 3:ncol(wellcomedata)])
   homo <- gtype[which(gtype != "H")]
   nwellcome[x, "CTC_A"] <- homo[1] ; nwellcome[x, "CTC_B"] <- homo[2]
-  nwellcome[x, which(as.character(wellcomedata[x, 3:ncol(wellcomedata)]) == nwellcome[x, "CTC_A"]) + 4] <- 0
-  nwellcome[x, which(as.character(wellcomedata[x, 3:ncol(wellcomedata)]) == "H") + 4] <- 1
-  nwellcome[x, which(as.character(wellcomedata[x, 3:ncol(wellcomedata)]) == nwellcome[x, "CTC_B"]) + 4] <- 2
+  nwellcome[x, which(gtypes == homo[1]) + 4] <- 0
+  nwellcome[x, which(gtypes == "H") + 4] <- 1
+  nwellcome[x, which(gtypes == homo[2]) + 4] <- 2
+  if((x %% 500) == 0) cat(x,"\n")
 }
-nwellcome <- data.frame(nwellcome, check.names = FALSE)
+nwellcome <- data.frame(nwellcome, check.names = FALSE, stringsAsFactors = FALSE)
+nwellcome[, 5:ncol(nwellcome)] <- apply(nwellcome[, 5:ncol(nwellcome)], 2, as.numeric)
 
+clusters   <- hclust(dist(t(nwellcome[sample(nrow(nwellcome), 100), 5:ncol(nwellcome)])))                                                       # 100 random SNPs
+dendrogram <- as.dendrogram(clusters)                                                                                                           # Dendrogram of all ALL lines
+par(cex = 0.2, font=3)
+plot(dendrogram)
 
-SNPregions <- apply(wellcomedata[1:10, c("CHR","POSITION","POSITION")], 1, paste0,collapse=":")
+corM <- matrix(0, nrow(nwellcome), nrow(nwellcome), dimnames = list(rownames(wellcomedata), rownames(wellcomedata)))
+for(x in 1:nrow(nwellcome)){
+  corV <- as.numeric(cor(as.numeric(nwellcome[x, 5:ncol(nwellcome)]), t(nwellcome[, 5:ncol(nwellcome)]), use="pair"))
+  corM[x, ] <- corV
+  if((x %% 50) == 0) cat(x, "\n")
+}
+write.table(round(corM, d = 2), "CTC/Analysis/genoCorrelation.txt", sep="\t", quote=FALSE)
 
-library(biomaRt)                                                                                                                    # Biomart
-snp.db <- useMart("snp", dataset="mmusculus_snp")                                                                                   # For mouse SNPs
+cat("", file="CTC/Analysis/toAnnotate.txt")
+for(x in 1:nrow(goodprobes)){
+  cat(as.character(goodprobes[x,"Chr"]), as.character(goodprobes[x,"Location"]), as.character(goodprobes[x,"Location"]), 
+      paste0(goodprobes[x,"Ref"],"/",goodprobes[x,"Alt"]),1, "\n",file="CTC/Analysis/toAnnotate.txt", append=TRUE)
+}
 
+### Use the VEP to annotate our file
 
 ourdata <- read.table(file="Analysis/measurementsALL_annotated.txt", sep="\t", header=TRUE,nrow=100)
 
-annotation <- read.table("Annotation/MouseAnnotation.txt", header=TRUE)                          # Load the annotation
-annInData  <- match(colnames(ourdata), rownames(annotation))                                     # Align JAX B6 with the JAX BMMI data
+annotation <- read.table("Annotation/MouseAnnotation.txt", header=TRUE)                                                                         # Load the annotation
+annInData  <- match(colnames(ourdata), rownames(annotation))                                                                                    # Align JAX B6 with the JAX BMMI data
 annotation <- annotation[annInData,]
-cat("Our data loaded:", ncol(ourdata), "strains,", nrow(ourdata), "SNPs\n")                      # Our data loaded: 86 strains, 543047 SNPs
+cat("Our data loaded:", ncol(ourdata), "strains,", nrow(ourdata), "SNPs\n")                                                                     # Our data loaded: 86 strains, 543047 SNPs
 
 chromosomes  <- as.character(c(1:19, "X", "Y", "M"))
 chrInfo      <- read.table("Annotation/mouseChrInfo.txt", header=TRUE)

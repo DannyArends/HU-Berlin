1da2e7e889a0d66e57dae3fe85cedea980782b42
DannyArends <danny.arends@gmail.com>
2015-06-10 12:06:25 +0200

    Adding permutation

--- Shijie Lyu/BootStrap.R
+++ Shijie Lyu/BootStrap.R
@@ -14,7 +14,7 @@ for(x in 1:100){
   gS[round(runif(1,1,100))] <- gS[round(runif(1,1,100))]
 }
 
-phenotypes <- rnorm(200) + 1.5 * genotypes[,15]
+phenotypes <- rnorm(200) + 0.5 * genotypes[,15]
 
 mapQTL <- function(genotypes,phenotypes){
   pvalues <- NULL
@@ -24,16 +24,22 @@ mapQTL <- function(genotypes,phenotypes){
   pvalues
 }
 
+QTLs <- -log10(mapQTL(genotypes, phenotypes))
+
 boots <- NULL
 for(x in 1:100){
   idx <- sample(nrow(genotypes), 200, replace=TRUE)
   boots <- rbind(boots, mapQTL(genotypes[idx, ], phenotypes[idx]))
 }
-
-QTLs <- -log10(mapQTL(genotypes, phenotypes))
-
 top <- apply(-log10(boots), 2, function(x){ sort(x)[97] })
 bot <- apply(-log10(boots), 2, function(x){ sort(x)[2] })
+
+perms <- NULL
+for(x in 1:100){
+  perms <- rbind(perms, mapQTL(genotypes[sample(nrow(genotypes)), ], phenotypes[idx]))
+}
+threshold <- apply(-log10(perms), 2, function(x){ sort(x)[97] })
+maxes <- apply(-log10(perms), 1, function(x){ max(x) })
 
 plot(c(0,100), c(0,45), t='n')
 points(QTLs, t='l', lwd=2)
@@ -42,6 +48,8 @@ points(top, t = 'h', col='red')
 points(QTLs ,t='h', col="blue")
 points(bot, t = 'l', col='blue')
 points(bot ,t='h', col="white")
+points(threshold, t='l', lwd=2, lty=3)
+abline(h=sort(maxes)[95], lwd=2,lty=3)
 
 abline(h=bot[which.max(QTLs)], lwd=2,lty=2)
 

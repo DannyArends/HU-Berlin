3a89d36b0aff611f6a9b4e1d4ea2936b379747a8
DannyArends <danny.arends@gmail.com>
2015-07-31 15:06:18 +0200

    Minor updates, adding some boxplots for Jaspers analysis

--- Jasper/analysis.R
+++ Jasper/analysis.R
@@ -3,11 +3,9 @@
 #
 #
 
-
 setwd("d:/Jasper")
 
 ## CD 3 - 7mm
-
 mdata <- read.table("CD3.data.txt",sep="\t",header=TRUE, row.names=1)
 
 for(x in 2:7){
@@ -17,27 +15,90 @@ for(x in 2:7){
   cat(colnames(mdata)[x], ":", pv1$p.value, pv2$p.value, pv3$p.value, "\n")
 }
 
+postscript("CD3.groups.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  plot(c(0.5,10), c(150,250),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD3 Gesamt",xaxt='n')
+  rect(0,0,3.5,1000,col="white")
+  rect(3.5, 0,6.5,1000,col="white")
+  rect(6.5,0,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortex.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortex.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortex.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+
+  plot(c(0.5,10), c(0,250),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD3",xaxt='n')
+  rect(0,-10,3.5,1000,col="white")
+  rect(3.5, -10,6.5,1000,col="white")
+  rect(6.5,-10,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.CD3"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.CD3"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.CD3"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.CD3"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.CD3"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.CD3"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortes.CD3"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortes.CD3"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortes.CD3"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+dev.off()
 
 mdata <- read.table("CD3.comparison.txt",sep="\t",header=TRUE)
 anova(lm(mdata[,"Gesamt"] ~ mdata[,"Group"] + mdata[,"location"]))
 anova(lm(mdata[,"CD3"] ~ mdata[,"Group"] + mdata[,"location"]))
 
+postscript("CD3.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  boxplot(CD3 ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n', main="CD3")
+  rect(0,0,2.5,100,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,100,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,100,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(CD3 ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+
+
+
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n',main="Gesamt CD3")
+  rect(0,0,2.5,1000,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,1000,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,1000,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+dev.off()
+
+postscript("legend.eps", horizontal=FALSE)
+  plot(1:10,t='n')
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),c("Keimzentrum Rand", "Keimzentrum"),bg="white")
+dev.off()
+
+
 # Conclusions:
-Combined Rand and Zentrum
-  - Gesamt measurement: Significant difference between locations: Rand versus Zentrum (P = 0.02398)
-  - Gesamt measurement : Significant difference between groups: 1, 2 and 3 (P = 0.08988)
+#Combined Rand and Zentrum
+#  - Gesamt measurement: Significant difference between locations: Rand versus Zentrum (P = 0.02398)
+#  - Gesamt measurement : Significant difference between groups: 1, 2 and 3 (P = 0.08988)
 
-  - CD3 measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 2.833e-10)
-  - CD3 measurement : Significant difference between groups: 1, 2 and 3 (P = 0.0608)
+#  - CD3 measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 2.833e-10)
+#  - CD3 measurement : Significant difference between groups: 1, 2 and 3 (P = 0.0608)
 
-- Single groups:
+#- Single groups:
 
-Keimzentrum.gesamt :      0.06082025.  0.534938  0.20149 
-Keimzentrum.CD3 :         0.1557976   0.6617248 0.1401711 
-Rand.gesamt :             0.2645785   0.9686234 0.3176751 
-Rand.CD3 :                0.02705842* 0.2712357 0.03868401*
-Paracortex.gesamt :       0.7522009   0.8958058 0.8237213 
-Paracortes.CD3 :          0.6903786   0.5806389 0.9534926 
+#Keimzentrum.gesamt :      0.06082025.  0.534938  0.20149 
+#Keimzentrum.CD3 :         0.1557976   0.6617248 0.1401711 
+#Rand.gesamt :             0.2645785   0.9686234 0.3176751 
+#Rand.CD3 :                0.02705842* 0.2712357 0.03868401*
+#Paracortex.gesamt :       0.7522009   0.8958058 0.8237213 
+#Paracortes.CD3 :          0.6903786   0.5806389 0.9534926 
 
 
 ## CD 4 - 7mm
@@ -50,25 +111,83 @@ for(x in 2:7){
   pv3 <- t.test(mdata[mdata[,"Group"] == 1, x], mdata[mdata[,"Group"] == 3, x])
   cat(colnames(mdata)[x], ":", pv1$p.value, pv2$p.value, pv3$p.value, "\n")
 }
+
+
+postscript("CD4.groups.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  plot(c(0.5,10), c(100,240),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD4 Gesamt",xaxt='n')
+  rect(0,0,3.5,1000,col="white")
+  rect(3.5, 0,6.5,1000,col="white")
+  rect(6.5,0,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortex.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortex.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortex.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+
+  plot(c(0.5,10), c(0,250),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD4",xaxt='n')
+  rect(0,-10,3.5,1000,col="white")
+  rect(3.5, -10,6.5,1000,col="white")
+  rect(6.5,-10,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.CD4"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.CD4"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.CD4"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.CD4"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.CD4"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.CD4"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortes.CD4"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortes.CD4"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortes.CD4"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+dev.off()
+
 mdata <- read.table("CD4.comparison.txt",sep="\t",header=TRUE)
 anova(lm(mdata[,"Gesamt"] ~ mdata[,"Group"] + mdata[,"location"]))
 anova(lm(mdata[,"CD4"] ~ mdata[,"Group"] + mdata[,"location"]))
 
+postscript("CD4.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  boxplot(CD4 ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n', main="CD4")
+  rect(0,0,2.5,100,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,100,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,100,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(CD4 ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n',main="Gesamt CD4")
+  rect(0,0,2.5,1000,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,1000,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,1000,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+dev.off()
+
 # Conclusions:
-Combined Rand and Zentrum
-  - Gesamt measurement: NO Significant difference between locations: Rand versus Zentrum (P = 0.74364)
-  - Gesamt measurement : Significant difference between groups: 1, 2 and 3 (P = 0.06467)
-  
-  - CD4 measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 0.000192)
-  - CD4 measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.286769)
-  
-                              1-2       2-3       1-3
-    Keimzentrum.gesamt :    0.1591979 0.1931342 0.3441863 
-    Keimzentrum.CD4 :       0.4293258 0.9608805 0.4443296 
-    Rand.gesamt :           0.1846171 0.9146737 0.194292 
-    Rand.CD4 :              0.6478391 0.7207282 0.8291717 
-    Paracortex.gesamt :     0.4211689 0.4983198 0.6286881 
-    Paracortes.CD4 :        0.6636186 0.7143741 0.8233504 
+#Combined Rand and Zentrum
+#  - Gesamt measurement: NO Significant difference between locations: Rand versus Zentrum (P = 0.74364)
+#  - Gesamt measurement : Significant difference between groups: 1, 2 and 3 (P = 0.06467)
+#  
+#  - CD4 measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 0.000192)
+#  - CD4 measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.286769)
+#  
+#                              1-2       2-3       1-3
+#    Keimzentrum.gesamt :    0.1591979 0.1931342 0.3441863 
+#    Keimzentrum.CD4 :       0.4293258 0.9608805 0.4443296 
+#    Rand.gesamt :           0.1846171 0.9146737 0.194292 
+#    Rand.CD4 :              0.6478391 0.7207282 0.8291717 
+#    Paracortex.gesamt :     0.4211689 0.4983198 0.6286881 
+#    Paracortes.CD4 :        0.6636186 0.7143741 0.8233504 
 
 
 ## CD 8a - 7mm
@@ -82,25 +201,83 @@ for(x in 2:7){
   cat(colnames(mdata)[x], ":", pv1$p.value, pv2$p.value, pv3$p.value, "\n")
 }
 
+
+postscript("CD8a.groups.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  plot(c(0.5,10), c(100,220),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD8a Gesamt",xaxt='n')
+  rect(0,0,3.5,1000,col="white")
+  rect(3.5, 0,6.5,1000,col="white")
+  rect(6.5,0,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortex.gesamt"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortex.gesamt"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortex.gesamt"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+
+  plot(c(0.5,10), c(0,250),t='n',xlab="Gewebe", ylab="Zellzahl", main="CD8a",xaxt='n')
+  rect(0,-10,3.5,1000,col="white")
+  rect(3.5, -10,6.5,1000,col="white")
+  rect(6.5,-10,100,1000,col="white")
+  boxplot(at=1, mdata[mdata[,"Group"] == 1,"Keimzentrum.CD8a"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=2, mdata[mdata[,"Group"] == 2,"Keimzentrum.CD8a"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=3, mdata[mdata[,"Group"] == 3,"Keimzentrum.CD8a"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=4, mdata[mdata[,"Group"] == 1,"Rand.CD8a"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=5, mdata[mdata[,"Group"] == 2,"Rand.CD8a"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=6, mdata[mdata[,"Group"] == 3,"Rand.CD8a"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  
+  boxplot(at=7, mdata[mdata[,"Group"] == 1,"Paracortes.CD8a"],add=TRUE,col=rgb(0.3,0.3,0.3))
+  boxplot(at=8, mdata[mdata[,"Group"] == 2,"Paracortes.CD8a"],add=TRUE,col=rgb(0.6,0.6,0.6))
+  boxplot(at=9, mdata[mdata[,"Group"] == 3,"Paracortes.CD8a"],add=TRUE,col=rgb(0.8,0.8,0.8))
+  axis(1, at=c(2, 5, 8),c("Keimzentrum","Rand", "Paracortex"))
+  legend("topleft", fill=c(rgb(0.3,0.3,0.3), rgb(0.6,0.6,0.6), rgb(0.8,0.8,0.8)),c("Gruppe 1", "Gruppe 2", "Gruppe 3"),bg="white")
+dev.off()
+
+
 mdata <- read.table("CD8a.comparison.txt",sep="\t",header=TRUE)
 anova(lm(mdata[,"Gesamt"] ~ mdata[,"Group"] + mdata[,"location"]))
 anova(lm(mdata[,"CD8a"] ~ mdata[,"Group"] + mdata[,"location"]))
 
 
-# Conclusions:
-Combined Rand and Zentrum
-  - Gesamt measurement: No Significant difference between locations: Rand versus Zentrum (P = 0.9682)
-  - Gesamt measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.1221)
+postscript("CD8a.eps", horizontal=FALSE)
+  op <- par(mfrow=c(2,1))
+  boxplot(CD8a ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n', main="CD8a")
+  rect(0,0,2.5,100,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,100,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,100,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(CD8a ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
 
-  - CD8a measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 0.0003082)
-  - CD8a measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.1894574)
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),xaxt='n',main="Gesamt CD8a")
+  rect(0,0,2.5,1000,col=rgb(0.3,0.3,0.3,0.5))
+  rect(2.5,0,4.5,1000,col=rgb(0.6,0.6,0.6,0.5))
+  rect(4.5,0,9,1000,col=rgb(0.9,0.9,0.9,0.5))
+  boxplot(Gesamt ~ location + Group,data = mdata, col = c(rgb(0.3,0.3,0.3), rgb(0.7,0.7,0.7)),add=TRUE,xaxt='n')
+  axis(1, at=c(1.5, 3.5, 5.5), c("Gruppe 1", "Gruppe 2", "Gruppe 3"))
+dev.off()
+
+# Conclusions:
+#Combined Rand and Zentrum
+#  - Gesamt measurement: No Significant difference between locations: Rand versus Zentrum (P = 0.9682)
+#  - Gesamt measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.1221)
+
+#  - CD8a measurement:  Highly Significant difference between locations: Rand versus Zentrum (P = 0.0003082)
+#  - CD8a measurement : No Significant difference between groups: 1, 2 and 3 (P = 0.1894574)
   
-                              1-2       2-3       1-3
-    Keimzentrum.gesamt :  0.0713338.   0.4603786  0.1981134 
-    Keimzentrum.CD8a :    0.05088885.  0.2596316  0.2802181 
-    Rand.gesamt :         0.02281575*  0.3997435  0.1953908 
-    Rand.CD8a :           0.5197883    0.696515   0.4267687 
-    Paracortex.gesamt :   0.4083458    0.4029569  0.8934307 
-    Paracortes.CD8a :     0.72508      0.4487628  0.3236331 
-    
-    
+#                              1-2       2-3       1-3
+#    Keimzentrum.gesamt :  0.0713338.   0.4603786  0.1981134 
+#    Keimzentrum.CD8a :    0.05088885.  0.2596316  0.2802181 
+#    Rand.gesamt :         0.02281575*  0.3997435  0.1953908 
+#    Rand.CD8a :           0.5197883    0.696515   0.4267687 
+#    Paracortex.gesamt :   0.4083458    0.4029569  0.8934307 
+#    Paracortes.CD8a :     0.72508      0.4487628  0.3236331 
+
--- Mouse/Muga/2000individuals.R
+++ Mouse/Muga/2000individuals.R
@@ -1,8 +1,7 @@
 setwd("E:/Mouse/DNA/MegaMuga")
-chr12 <- read.table("chromsome12.txt", sep="\t", header=TRUE, check.names=FALSE,na.strings=c(""))
+chr12 <- read.table("chromsome12.txt", sep="\t", header=TRUE, check.names=FALSE,na.strings=c("","-"))
 map <- t(chr12[1:2,])[-1,]
 chr12 <- chr12[-c(1,2),]
-
 
 individualsHaveOne <- as.character(chr12[which(apply(apply(chr12[,2:4],1,is.na),2,sum) != 3),1])
 individualsHaveOne <- individualsHaveOne[-which(grepl("B", individualsHaveOne))]
@@ -32,8 +31,8 @@ chr12a <- cbind(as.character(chr12[,1]), chr12a)
 
 rownames(genotypesOG) <- colnames(genotypesO)
 
-strt <-  89085109
-stp <-  strt + 11891872
+strt <-  82085109
+stp <-  strt + 18891872
 
 inR <- rownames(mapO[which(mapO[,1] == 12 & as.numeric(mapO[,2]) > strt & as.numeric(mapO[,2]) < stp),])
 
@@ -46,8 +45,22 @@ haveScore <- rownames(qtl[inR,][which(!is.na(qtl[inR, "marker"])),])
 genotypesN <- chr12a[which(chr12a[,1] %in% individualsHaveOne),]
 rownames(genotypesN) <- genotypesN[,1]
 genotypesN <- cbind(map, t(genotypesN[,-1]))
+colnames(genotypesN)[1:2] <- c("Chr","Mb_NCBI38")
 
 genotypesOO <- genotypesOG[individualsHaveOne,haveScore]
 genotypesOO <- cbind(mapO[colnames(genotypesOO),c(1,2)],t(genotypesOO))
 
 #Bind together
+
+combined <- rbind(genotypesOO,genotypesN)
+combined <- combined[sort(as.numeric(combined[,2]),index.return=TRUE)$ix,]
+combined <- cbind(LOD = qtl[rownames(combined),"marker"],combined)
+
+
+write.table(combined,file="GenotypesChr12.txt",sep="\t")
+
+phased <- genotypesPh[rownames(combined),]
+phased <- phased[,-which(apply(phased, 2, function(x){return(sum(is.na(x)))}) > 70)]
+
+phased <- cbind(LOD = qtl[rownames(phased),"marker"],phased)
+write.table(phased,file="GenotypesChr12Phased.txt",sep="\t")

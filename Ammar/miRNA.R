007d24584e0d701568955f236f56fe84cdb78f5e
DannyArends <danny.arends@gmail.com>
2015-09-03 16:43:59 +0200

    miRNA in cow updates

--- Ammar/miRNA.R
+++ Ammar/miRNA.R
@@ -4,6 +4,34 @@
 # last modified Aug, 2015
 # first written Aug, 2015
 
+## Miranda
+
+setwd("E:/Cow/RNA/miRNA")
+mdata <- read.table("miRNA.txt",sep="\t",header=TRUE)     # Data from mirbase
+
+miRNA.bta <- mdata[which(grepl("bta", mdata[,"ID"])),]
+cat("", file="cow.miRNA.fasta")
+
+for(x in 1:nrow(miRNA.bta)){
+  cat(paste0(">", miRNA.bta[x,"Mature1_ID"], "\n"), file="cow.miRNA.fasta",append=TRUE)
+  cat(paste0(miRNA.bta[x,"Mature1_Seq"], "\n"), file="cow.miRNA.fasta",append=TRUE)
+  if(miRNA.bta[x,"Mature2_ID"]!=""){
+    cat(paste0(">", miRNA.bta[x,"Mature2_ID"], "\n"), file="cow.miRNA.fasta",append=TRUE)
+    cat(paste0(miRNA.bta[x,"Mature2_Seq"], "\n"), file="cow.miRNA.fasta",append=TRUE)
+  }
+}
+
+execute("./miRanda-3.3a/src/miranda cow.miRNA.fasta reference.txt | grep '^>bta' > scanout.txt", "scanout.txt")
+
+scan.results <- read.table("scanout.txt")
+scan.results[,1] <-  gsub(">bta","bta",scan.results[,1])
+colnames(scan.results) <- c("miRNA_id", "gene_id","score", "energy","from.miRNA","to.miRNA", "from.UTR", "to.UTR", "length", "alignment%", "alignment max%")
+
+write.table(scan.results[which(scan.results[,"energy"] < -10),],"miRNA.scan.output",sep="\t",row.names=FALSE)
+
+
+## PITA
+
 # Execute function, does not execute when outputfile exists
 execute <- function(x, outputfile = NA, intern = FALSE){
   if(!is.na(outputfile) && file.exists(outputfile)){ cat("Output for step exists, skipping this step\n"); return("") }

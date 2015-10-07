dbdf7d75b7f61436fa2f80e9c86da1ddc0f15663
DannyArends <danny.arends@gmail.com>
2015-10-07 10:09:16 +0200

    Do not print the stuff which is already done, to prevent us from doing RNASeq analysis double

--- Mouse/RNASeq2/gencmd.R
+++ Mouse/RNASeq2/gencmd.R
@@ -6,8 +6,11 @@
 
 setwd("E:/Mouse/RNA/Sequencing/Reciprocal Cross B6 BFMI by MPI/Experiment")
 samples <- read.table("sampledescription.txt", sep="\t", header=TRUE, colClasses="character")
+done <- c("4422","4423"  ,"5080"  ,"4424"  ,"5081"  ,"5079"  ,"5076"  ,"5078"  ,"5077"  ,"5075"  ,"5072"  ,"5070"  ,"5068"  ,"5071"  ,"6345"  ,"6339"  ,"6341"  ,"6343"  ,"6344"  ,"5073"  ,"4427"  ,"6346"  ,"6342" )
 
 for(x in 1:nrow(samples)){
   fname <- paste0(samples[x,"Lib_id"], "_", samples[x,"TagLane"])
-  cat(paste0("nohup Rscript pipeline.R /home/arends/NAS/Mouse/RNA/Sequencing/ReciprocalCrossB6xBFMI/FASTQ/", fname," > ",fname,".log 2>&1&"),"\n")
+  if(!samples[x,"Lib_id"] %in% done){
+    cat(paste0("nohup Rscript pipeline.R /home/arends/NAS/Mouse/RNA/Sequencing/ReciprocalCrossB6xBFMI/FASTQ/", fname," > ",fname,".log 2>&1&"),"\n")
+  }
 }

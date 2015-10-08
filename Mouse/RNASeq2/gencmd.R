f09cae2ee3c9491edd63fcd5555e38a3774ddcb8
DannyArends <danny.arends@gmail.com>
2015-10-08 15:43:42 +0200

    Adding more samples to Done

--- Mouse/RNASeq2/gencmd.R
+++ Mouse/RNASeq2/gencmd.R
@@ -6,7 +6,7 @@
 
 setwd("E:/Mouse/RNA/Sequencing/Reciprocal Cross B6 BFMI by MPI/Experiment")
 samples <- read.table("sampledescription.txt", sep="\t", header=TRUE, colClasses="character")
-done <- c("4422","4423"  ,"5080"  ,"4424"  ,"5081"  ,"5079"  ,"5076"  ,"5078"  ,"5077"  ,"5075"  ,"5072"  ,"5070"  ,"5068"  ,"5071"  ,"6345"  ,"6339"  ,"6341"  ,"6343"  ,"6344"  ,"5073"  ,"4427"  ,"6346"  ,"6342" )
+done <- c("4422", "4426","4423"  ,"5080"  ,"4424"  ,"5081"  ,"5079"  ,"5076"  ,"5078"  ,"5077"  ,"5075"  ,"5072"  ,"5070"  ,"5068"  ,"5071"  ,"6345"  ,"6339"  ,"6341"  ,"6343"  ,"6344"  ,"5073"  ,"4427"  ,"6346"  ,"6342" )
 
 for(x in 1:nrow(samples)){
   fname <- paste0(samples[x,"Lib_id"], "_", samples[x,"TagLane"])
